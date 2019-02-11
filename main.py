b37bdfb42af5e42a2c1ee88e2db4c64e7ed7c944
PierreBouffartigue <43409971+PierreBouffartigue@users.noreply.github.com>
2019-02-11 16:39:45 +0100

    Argparse -help

--- main.py
+++ main.py
@@ -18,13 +18,18 @@ Fichier_MD = open(MD,'r')
 #Ecriture de la mise en page html dans le nouveau fichier html avec au milieu une conversion du markdown en html 
 
 print("Conversion du Markdown en HTML ...")
+
 Fichier_HTML.write("<!DOCTYPE html>\n<html>\n <head>\n\t<meta charset='utf-8'/>\n\t<title>Site-Statique</title>\n\t<link rel='stylesheet' href='style.css' />\n </head>\n <body>\n")
 lecture = Fichier_MD.read()
 Fichier_HTML.write(md.convert(lecture))
 Fichier_HTML.write("\n </body>\n</html>")
+
 print("Fin de la conversion !")
 
 #Fermeture du fichier et fin du programme
+
+parser = argparse.ArgumentParser
+parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit.')
 
 Fichier_HTML.close()
 
