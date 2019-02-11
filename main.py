a439bc2f66d81c85ae0bf2889490acc4770fae18
PierreBouffartigue <43409971+PierreBouffartigue@users.noreply.github.com>
2019-02-11 15:57:16 +0100

    main plus clair

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ md = markdown.Markdown()
 #Préparation à l'écriture et la lisibilité des fichiers 
 HTML = 'test.html'
 MD = 'replace_me.md'
-Fichier = open(HTML,'w')
-Fichier2 = open(MD,'r')
+Fichier_HTML = open(HTML,'w')
+Fichier_MD = open(MD,'r')
 
 #Ecriture de la mise en page html dans le nouveau fichier html avec au milieu conversion du markdown en html 
 
-Fichier.write("<!DOCTYPE html>\n<html>\n <head>\n\t<meta charset='utf-8'/>\n\t<title>Site-Statique</title>\n\t<link rel='stylesheet' href='style.css' />\n </head>\n <body>\n")
-lecture = Fichier2.read()
-Fichier.write(md.convert(lecture))
-Fichier.write("\n </body>\n</html>")
+Fichier_HTML.write("<!DOCTYPE html>\n<html>\n <head>\n\t<meta charset='utf-8'/>\n\t<title>Site-Statique</title>\n\t<link rel='stylesheet' href='style.css' />\n </head>\n <body>\n")
+lecture = Fichier_MD.read()
+Fichier_HTML.write(md.convert(lecture))
+Fichier_HTML.write("\n </body>\n</html>")
 
 #Fermeture du fichier et fin du programme
 
-Fichier.close()
+Fichier_HTML.close()
 
